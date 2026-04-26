be65ac3aa4da5d4b5eb245bc8f06acf2011333a2
Weller <pelu.kiefer@gmail.com>
2026-04-26 18:17:30 -0500

    api

--- API/main.py
+++ API/main.py
@@ -1,15 +1,41 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class ItemOut(BaseModel):
+    name: str
+    price: float
+    tax: float | None = None
+
+class ItemIn(ItemOut):
+    internal_code: str
+
+products = {
+    "a1": {
+        "name": "Laptop",
+        "price": 1000,
+        "internal_code": "XYZ123"
+    },
+    "b2": {
+        "name": "Mouse",
+        "price": 50,
+        "tax": 5,
+        "internal_code": "ABC999"
+    }
+}
 
 app = FastAPI()
 
+producto = 1;
 
-ffd = []
+@app.get("/items/{item_id}",response_model=ItemOut,response_model_exclude_unset=True)
+async def pedir(item_id:str):
+    if item_id not in products:
+        return {"mensaje":"No existe el producto"}
+    return products[item_id]
 
-@app.get("/")
-async def login():
-    return {"mensaje":"chao"}
-
-@app.get("/")
-async def login():
-    return {"mensaje":"jih"}
-
+@app.post("/items/",response_model=ItemOut,response_model_exclude_unset=True)
+async def crear(item:ItemIn):
+    global producto
+    products[str(producto)] = item.model_dump()
+    producto = producto + 1
+    return item
