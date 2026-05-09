7a02fb90d04e17ea8e0c8f73e3be1948bc7e2165
Weller <pelu.kiefer@gmail.com>
2026-05-09 03:22:13 -0500

    fix CORS for vercel

--- API/main.py
+++ API/main.py
@@ -43,8 +43,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
